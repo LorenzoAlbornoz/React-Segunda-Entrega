88a2650248576ae7a0515e91c1081bc6ac3c42b9
LorenzoAlbornoz <lorenzoa.28.la@gmail.com>
2022-05-06 12:36:07 -0300

    Boton

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import {useState} from 'react'
+import { FaBeer } from 'react-icons/fa';
+
 //import Titulo from "./components/Titulo/Titulo";
 //import Formulario from "./components/Formulario/Formulario";
 //import FormContainer from "./components/componenteContenedor/FormContainer";
+
 
 
 import "./App.css";
@@ -8,17 +12,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
+
 function App() {
+  const [count, setCount] = useState(1)
+  const [fecha, setFecha] = useState(Date())
+  
+
+  //console.log(arrayEstado)
   //const tit = "Soy titulo de App";
   //const subTit = "Soy subtitulo de App";
+  
+  function sumar  () {
+    //count ++
+    setCount(count + 1)
+    setFecha(Date(fecha))
+    //console.log (count)
+  }
+
+  function restar () {
+    setCount (count - 1)
+  }
+
+
 
   return (
     <div className="App">
-      <NavBar />
-      <ItemListContainer greeting={'Hola soy el componente contenedor'}/>
-
-    </div>
-  );
-}
-
-export default App;
+      <NavBar 
+      componente = {() => ItemListContainer ({ saludo:  'Hola soy el componente contenedor'})}
+      >
+        {/*<ItemListContainer greeting={'Hola soy el componente contenedor'}/>*/}
+        </NavBar>
+        
+        <p>Unidades disponibles: </p>
+        <p>Fecha de la compra: {fecha}</p>
+        <p>{count}</p>
+        
+        <button onClick={sumar}>Sumar +</button>
+        <button onClick={restar}>Restar -</button>
+       <contador initial ={1} stock={5} onAdd/>
+      </div>
+    );
+  }
+  
+  export default App;
